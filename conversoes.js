87ec368e04989992dda3d674b8d86e5b554b408c
Murillo Zubk <mzubk.dev@gmail.com>
2023-09-25 16:03:10 -0300

    codigos var e conversões

---
+++ conversoes.js
@@ -0,0 +1,14 @@
+// tipos de dados
+// booleans
+
+// conversão implícita
+
+const num = 456;
+const numString = Number("456a");
+
+// Number() converte string para número
+// String() converte número para string
+
+console.log(num + numString);
+
+// conversão explicita
