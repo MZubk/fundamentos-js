af65e0769a9d648c0b55bf31f02bbf23f3fd627c
Murillo Zubk <mzubk.dev@gmail.com>
2023-09-26 16:41:55 -0300

    operadores

---
+++ comparacoes/comparacoes.js
@@ -0,0 +1,21 @@
+// == comparação implícita
+
+const numero = 5;
+const texto = "5";
+
+console.log(numero == texto); // retorna true
+
+console.log(numero === texto); // retorna false
+
+// typeof
+
+console.log(typeof numero); // retorna number
+console.log(typeof texto); // retorna string
+
+// == compara o valor
+// === compara o tipo
+
+// conversão explicita
+
+Number();
+String();
