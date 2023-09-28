1dc124e817c16eab0a6267bd512b65cd991d74c1
Murillo Zubk <mzubk.dev@gmail.com>
2023-09-27 22:19:48 -0300

    funções e paramentros

---
+++ functions/script.js
@@ -0,0 +1,24 @@
+// let x = "";
+// console.log(x);
+// x = "oi";
+
+// 1 - declaração de função
+
+// nome  // parâmetro
+function imprimeTexto(texto) {
+  console.log(texto); // chamada da função
+}
+
+// 2 - executa a função (1 ou + vezes)
+// imprimeTexto("ola");
+// imprimeTexto("mundo");
+
+imprimeTexto(soma());
+
+// tres formas de escrever funções
+
+function soma() {
+  return (resultado = 2 + 2);
+}
+
+// console.log(soma());
---
+++ parameters/script.js
@@ -0,0 +1,17 @@
+function soma(a = 0, b = 0) {
+  return a + b;
+}
+
+// console.log(soma(5, 5));
+
+// function nomeIdade(nome, idade) {
+//   return `meu nome é ${nome} e minha idade é ${idade} anos`;
+// }
+
+// console.log(nomeIdade("Murillo", 30));
+
+function multiplica(a = 1, b = 1) {
+  return a * b;
+}
+
+console.log(multiplica(soma(4)));
